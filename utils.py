bdb6e00a3ee12c7811f30107d97ece72352f0147
Snehal Ranjan <snehalranjan5472@gmail.com>
2024-09-23 14:59:06 +0530

    General utils

---
+++ utils.py
@@ -0,0 +1,16 @@
+def seconds_to_hours_minutes(seconds):
+        hours, remainder = divmod(seconds, 3600)
+        minutes, _ = divmod(remainder, 60)
+        return f"{int(hours)}:{int(minutes)}"
+
+def create_formatted_transcripts(df):
+    transcripts = ""
+    for i in range(len(df)):
+        transcripts += f"""
+        <transcript>
+            <start_time>{seconds_to_hours_minutes(df.iloc[i].start)}</start_time>
+            <end_time>{seconds_to_hours_minutes(df.iloc[i].end)}</end_time>
+            <text>{df.iloc[i].text}</text>
+        </transcript>
+        """
+    return transcripts
